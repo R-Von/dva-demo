2f749760909d7e43da58b0dc70371099d1d2a009
R-Von <778615648@qq.com>
2020-01-08 18:34:43 +0800

    feat: update the pages

--- dva-quickstart/src/routes/Products.js
+++ dva-quickstart/src/routes/Products.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { connect } from 'dva'
+import ProductList from '../components/ProductList'
 
-const Products = (props)=>{
+const Products = ({ dispatch, products })=>{
+  function handleDelete(id){
+    dispatch({
+      type: 'products/delete',
+      payload: id
+    })
+  }
   return (
-    <h2>List of Products</h2>
+    <div> 
+      <h2>List of Products</h2>
+      <ProductList onDelete={handleDelete} products={products} />
+    </div>
+    
   )
 }
 
-export default Products
+// export default Products
+export default connect(({ products })=>({
+  products
+}))(Products)
